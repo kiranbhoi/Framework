1ff80ed998b448ed44125d317f134476cffd6ee7
sumashri07 <110187336+sumashri07@users.noreply.github.com>
2022-07-28 23:24:39 -0700

    framework
    hii
    i  am done the changes

--- FrameWork1/src/main/java/com/qa/Pagelayer/TestBase.java
+++ FrameWork1/src/main/java/com/qa/Pagelayer/TestBase.java
@@ -3,8 +3,7 @@ package com.qa.Pagelayer;
 public class TestBase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("sumashri");
 	}
 
 }
--- FrameWork1/src/test/java/com/qa/testLayer/HomePage.java
+++ FrameWork1/src/test/java/com/qa/testLayer/HomePage.java
@@ -3,7 +3,7 @@ package com.qa.testLayer;
 public class HomePage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello");
 
 	}
 
